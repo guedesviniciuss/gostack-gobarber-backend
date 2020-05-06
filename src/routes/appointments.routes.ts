245c5034d752fc6a74d019823980eda0b6ab0909
Viniciusguedes <viniciusguedes@lavid.ufpb.br>
2020-05-06 01:43:20 -0300

    Leassons about SOLID

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.status(200).json(appointments);
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return res.status(200).json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`⚡ Server started on port ${PORT}.`);
